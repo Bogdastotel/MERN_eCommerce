db35d86699d3beb443ad509f195ad7dcc08b18a8
unknown <nikola91bogdanovic@gmail.com>
2021-07-18 17:20:49 +0200

    rating added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React  from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 
@@ -13,7 +14,7 @@ const App = () => {
 
         <main className='py-3'>
           <Container>
-          <h1>Welcome to ProShop</h1>
+              <HomeScreen/>
           </Container>
         </main>
        <Footer/>
