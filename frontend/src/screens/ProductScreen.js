80147cac92ece09b42909c888bf22272f72acd6e
unknown <nikola91bogdanovic@gmail.com>
2021-07-23 01:17:46 +0200

    front and back connected for products and home screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,68 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import Loader from 'react-loader-spinner'
 
 export const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState({})
 
-    const product = products.find(p => p._id === match.params.id )
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
 
+      setProduct(data)
+    }
+    fetchProducts()
+  }, [match.params.id])
 
-    return (
-        <>
-            <Link className='btn btn-light my-3'  to='/'>Go back</Link>
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: ${product.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: ${product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Price:
-                                    </Col>
-                                    <Col>
-                                        <strong>${product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Status:
-                                    </Col>
-                                    <Col>
-                                        <strong>{product.countInStock > 0 ? 'In stock' : 'Out of stock'}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button disabled={product.countInStock === 0} className='btn-block' type='button'>Add to cart</Button>
-                            </ListGroup.Item>
-
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    )
+  return Object.keys(product).length !== 0 ? (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  disabled={product.countInStock === 0}
+                  className='btn-block'
+                  type='button'
+                >
+                  Add to cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  ) : (
+    <Loader type='Puff' color='#00BFFF' height={100} width={100} />
+  )
 }
